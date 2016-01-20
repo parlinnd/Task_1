416c15ca5c4ced4eef3021747269958354af62e5
Parlin Nando IF3801 <parlinnando98@gmail.com>
2016-01-20 08:07:00 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,37 @@
+/*
+Parlin Nando (1103132235)
+IF-38-01
+ */
+
+import java.util.scanner;
+public class prima {
+    
+    public static void main(String[] args)
+    {
+        int angka;
+        int i = 2;
+        
+        scanner input = new scanner (system.in);
+        system.out.println("masukan angka : ");
+        angka = input.nextInt();
+        
+        boolean cek = true;
+        
+        while (i < angka)
+        {
+            if (angka & 1 = = 0)
+            {
+                cek = false;
+                break;
+            }
+            i++;
+        }
+        if (cek)
+            system.out.println("bilangan" + angka + "adalah bilangan prima ");
+        else
+            system.out.println("bilangan" + angka + "bukan bilangan prima");
+        
+    }
+    
+}
 
